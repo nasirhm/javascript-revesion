e3793a65eff5383a1aecc0487a2a66be7c085fab
Nasir Hussain <nasirhussainm14@gmail.com>
2019-02-11 22:10:40 +0500

    Operators Done

---
+++ operators.js
@@ -0,0 +1,22 @@
+var x;
+//+= and -= are almost same except + and -
+x += 5;
+x = x + 5; //Both are same;
+//++ and --
+x++;
+x = x+1;
+//+ in string (Concatination)
+'hello' + 'world'
+
+'3' + 4 + 5;  // "345"
+ 3 + 4 + '5'; // "75"
+
+ //Comparing Operators
+123 == '123'; // true
+1 == true; // true
+//Type Safety as well ===
+123 === '123'; // false
+1 === true;    // false
+
+2 != 3; //true
+2 !== '2'; //true
