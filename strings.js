35c41fb80c46edc8486e9e9bbecbd35dfdfc93d1
Nasir Hussain <nasirhussainm14@gmail.com>
2019-02-11 21:19:41 +0500

    Strings Done

---
+++ strings.js
@@ -0,0 +1,13 @@
+//To Find length add .length
+'hello'.length; //5
+
+//To find character at a specific index can be found by .charAt
+'hello'.charAt(0);
+
+//To Replace Stuff you can use .replace
+'hello, world'.replace('world','pakistan') //'hello, pakistan'
+
+'hello'.toUpperCase(); //'HELLO'
+'HELLO'.toLowerCase(); //'hello'
+'HELLO'.toLocaleLowerCase() //'hello'
+//The Difference with Locale is it can be used to convert differet locales( Languages )
