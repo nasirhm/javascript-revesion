f550f618c55205ee7934abfed656359418b5b783
Nasir Hussain <nasirhussainm14@gmail.com>
2019-02-11 21:37:34 +0500

    OthersJs done and Variables init

---
+++ other.js
@@ -0,0 +1,13 @@
+//Undefined and Null are different if i initialize a variable without declaration it is shown as undefined
+let a; //undefined
+a = null; //null
+
+//Boolean is a Data type in js as well
+//Boolean Rules:
+/* 
+false, 0 , empty strings "", NaN, null and undefined all becomes false
+all other values are treated as true
+*/
+Boolean(''); //false
+Boolean('234'); //true
+
